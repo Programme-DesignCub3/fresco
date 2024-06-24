c631f1f86bb8fb80251dfbe0af98eddc0314e1a2
Syafwan000 <muhammadsyafwan04@gmail.com>
2024-06-24 15:41:21 +0700

    add: gtag script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="canonical" href="{{ URL::current() }}" />
     <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}" />
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_TAG_ID') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '{{ env('GOOGLE_TAG_ID') }}');
+    </script>
     @stack('top-scripts')
     @include('partials.meta')
     @include('partials.favicon')
