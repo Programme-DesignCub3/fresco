7c66ddae49b4c1cd8930f89986f378c8da014701
Syafwan000 <muhammadsyafwan04@gmail.com>
2025-02-24 12:11:29 +0700

    fix: remove unused featured_image_portrait from activity query

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -29,7 +29,7 @@ class ActivityController extends Controller
         $pages = $pageSettings->toArray();
 
         // Activity Resource
-        $activity = Activity::where('start_date', '<=', Carbon::now()->toDateString())->where('end_date', '>=', Carbon::now()->toDateString())->with('featured_image')->with('featured_image_portrait')->latest()->get();
+        $activity = Activity::where('start_date', '<=', Carbon::now()->toDateString())->where('end_date', '>=', Carbon::now()->toDateString())->with('featured_image')->latest()->get();
         $activity->transform(function ($act) {
             $act->image = $act->featured_image->url ?? null;
             return $act;
